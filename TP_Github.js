613ff565fd38f254147a305ea1f9b52e6da6871b
jerome liegeois <jerome.liegeois@alltricks.com>
2025-04-29 15:31:14 +0200

    fix node

--- TP_Github.js
+++ TP_Github.js
@@ -27,18 +27,25 @@ waitForKeyElements(".js-issue-labels [data-name='WIP']", () => {
 
 // Select the node that will be observed for mutations
 const targetNode = document.getElementById("files");
+if (targetNode) {
+    // Options for the observer (which mutations to observe)
+    const config = {attributes: true, childList: true, subtree: true};
 
-// Options for the observer (which mutations to observe)
-const config = {attributes: true, childList: true, subtree: true};
-
-// Callback function to execute when mutations are observed
-const callback = (mutationList, observer) => {
-    for (const mutation of mutationList) {
-        if (mutation.type === "childList") {
-            addColorGitHub($(targetNode))
+    // Callback function to execute when mutations are observed
+    const callback = (mutationList, observer) => {
+        for (const mutation of mutationList) {
+            if (mutation.type === "childList") {
+                addColorGitHub(targetNode)
+            }
         }
-    }
-};
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+}
 
 // Create an observer instance linked to the callback function
 const observer = new MutationObserver(callback);
